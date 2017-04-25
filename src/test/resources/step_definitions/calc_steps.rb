79b765ba72363eb44474721c3669a5f5ce1ef97c
gbulmersa <glen.bulmer@stateauto.com>
2017-04-25 14:50:04 -0400

    Add Ruby cucumber steps

---
+++ src/test/resources/step_definitions/calc_steps.rb
@@ -0,0 +1,38 @@
+#cucumber resources -r resources/step_definitions
+
+Given(/^the user selects a coverage of Bodily Injury$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the user also selects a (\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the user asks for the coverage amount to be calculated$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the coverage amount is (\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the user selects a coverage of Uninsured Motorists$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the user hasn't selected a coverage name of:$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the user is told that a coverage name hasn't been selected$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the user hasn't selected a coverage limit of <coverageLimit>$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the user is told that a coverage limit hasn't been selected$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
